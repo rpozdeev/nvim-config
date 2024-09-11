8f8f68eb31caedc2b16a4267d06ed4b68b48c72f
Roman I Pozdeev <ya.rpozdeev@yandex.ru>
2024-09-11 17:17:17 +0300

    добалнеы плагиры для работы с git

---
+++ lua/plugins/git.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  { "tpope/vim-fugitive" },
+  {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = {
+      "tpope/vim-fugitive",
+    },
+  },
+}
