78f61b3c869c57c97a9920ada999acd37e749e68
Поздеев Роман <rip@m2.ru>
2024-10-17 16:04:32 +0300

    добалено venv-selector (python)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -201,4 +201,19 @@ return {
     { "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
   },
   },
+  {
+    "linux-cultist/venv-selector.nvim",
+    branch = "regexp", -- Use this branch for the new version
+    cmd = "VenvSelect",
+    opts = {
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
+        },
+      },
+    },
+    --  Call config for python files and load the cached venv automatically
+    ft = "python",
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+  },
 }
