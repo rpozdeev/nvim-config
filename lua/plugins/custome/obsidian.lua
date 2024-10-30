a0bde8573c170ae16a5f85e6f59eb65acc99cb10
Поздеев Роман <rip@m2.ru>
2024-10-30 15:32:42 +0300

    добавлена поддержка obsidian

---
+++ lua/plugins/custome/obsidian.lua
@@ -0,0 +1,29 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  event = "VimEnter",
+  lazy = true,
+  ft = "markdown",
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      -- {
+      --   name = "work",
+      --   path = "~/vaults/work",
+      -- },
+    },
+  },
+}
